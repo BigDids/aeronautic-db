51d7c2292bc73e396e9900eb03e55242e3b6a865
Didjer Perroud <didierperroud@stephanes-mini.home>
2025-06-13 14:36:50 +0200

    feat: add models

--- aerodrome/models.py
+++ aerodrome/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class AD(models.Model):
+    nom = models.CharField(max_length=100)
+    code_oaci = models.CharField(max_length=4)
+    frequence_radio_mhz = models.FloatField()
+
+
+class Pistes(models.Model):
+    id_ad = models.ForeignKey(AD, on_delete=models.CASCADE)
+    designation = models.CharField(max_length=7)
+
+
+class Batiments(models.Model):
+    id_ad = models.ForeignKey(AD, on_delete=models.CASCADE)
+    nom = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)
+
+
+class Aprons(models.Model):
+    id_ad = models.ForeignKey(AD, on_delete=models.CASCADE)
+    id_batiment = models.ForeignKey(Batiments, on_delete=models.CASCADE)
+    type = models.CharField(max_length=100)
+
+
+class Carburants(models.Model):
+    id_ad = models.ForeignKey(AD, on_delete=models.CASCADE)
+    type = models.CharField(max_length=50)
+
+
+class Services(models.Model):
+    id_ad = models.ForeignKey(AD, on_delete=models.CASCADE)
+    type = models.CharField(max_length=100)
