a404d717fdb0a765d44e09ca18e8bc3a4fec5059
Didjer Perroud <didierperroud@stephanes-mini.home>
2025-06-17 14:04:54 +0200

    feat: add admin view for all models

--- aerodrome/admin.py
+++ aerodrome/admin.py
@@ -3,14 +3,38 @@ from django.contrib import admin
 from .models import *
 
 
-class ApronsInline(admin.StackedInline):
-    model = Apron
-    extra = 1
-
-
+@admin.register(AD)
 class ADAdmin(admin.ModelAdmin):
-    fields = ["name", "icao_code", "frequency_mhz"]
-    inlines = [ApronsInline]
+    list_display = ["name", "icao_code"]
+    search_fields = ["name", "icao_code"]
+    autocomplete_fields = ["frequencies"]
 
 
-admin.site.register(AD, ADAdmin)
+@admin.register(RadioFrequency)
+class RadioFrequencyAdmin(admin.ModelAdmin):
+    search_fields = ["frequency_mhz"]
+
+
+@admin.register(Runway)
+class RunwayAdmin(admin.ModelAdmin):
+    list_display = ["designation", "ad"]
+
+
+@admin.register(Building)
+class BuildingAdmin(admin.ModelAdmin):
+    list_display = ["name", "building_type", "ad"]
+
+
+@admin.register(Apron)
+class ApronAdmin(admin.ModelAdmin):
+    list_display = ["apron_type", "building", "ad"]
+
+
+@admin.register(Fuel)
+class FuelAdmin(admin.ModelAdmin):
+    list_display = ["fuel_type", "ad"]
+
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ["service_type", "ad"]
